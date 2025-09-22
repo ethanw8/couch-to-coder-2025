1c966fc47b123fa929ce1c0d967abc4ea957d04e
ethanw8 <60151238+ethanw8@users.noreply.github.com>
2025-09-22 13:01:38 +0100

    Add files via upload

---
+++ session_1/intro.js
@@ -0,0 +1,41 @@
+
+const firstName = "Ethan"
+const lastName = "W"
+
+let message = "Hello"
+
+console.log("Hello", firstName, lastName + '!') // testing out different datatypes, like string and characters.
+
+let age = 18;
+
+if (age < 18) {
+    console.log("Not old enough to drink")
+} else if (age >= 18) {
+    console.log("You are old enough to drink")
+}
+
+const numbers = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+for (i = 0; i < numbers; i++) {
+    if (i[numbers] % 3 == 0) {
+        i.append("Fizz")
+    }
+    else if (i[numbers] % 6 == 0) {
+        i.append("Buzz")
+    }
+
+    else if (i[numbers] % 9 == 0) {
+        i.append("FizzBuzz")
+    }
+
+    console.log(numbers)
+
+
+
+
+
+
+
+}
+
+
